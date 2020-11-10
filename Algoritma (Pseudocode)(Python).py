542d00fabb9b783d70576498c4f0df32ae22f46e
Muhammad Irfan Fahriza <73998088+irfanfhrz@users.noreply.github.com>
2020-11-10 12:43:24 +0700

    Update Algoritma (Pseudocode)(Python).py

--- Algoritma (Pseudocode)(Python).py
+++ Algoritma (Pseudocode)(Python).py
@@ -1,13 +1,9 @@
 4.	Implementasi No 2 dalam Bahasa Python dan Bahasa C
 
-Bahasa Python
-#Kuadrat dari Penjumlahan 2 bilangan integer
-print('')
-a=int (input("Masukkan nilai a : "))
-b=int (input("Masukkan nilai b : "))
-print('')
-def penjumlahan_kuadrat (a,b):
-    penjumlahan_kuadrat =  ( a + b ) **2
-    return penjumlahan_kuadrat
-print("hasil Penjumlahannya :",penjumlahan_kuadrat (a,b))
-
+#program menjumlahkan dan menguadratkan dua bilangan
+a=int(input("masukkan nilai a:"))
+b=int(input("masukkan nilai b:"))
+X=a+b
+print("hasil penjumlahan :",a,"+",b,"=",+X)
+Y=X*X
+print("hasil penguadratan :",X,"*",X,"=",+Y)
